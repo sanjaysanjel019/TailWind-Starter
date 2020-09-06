717b3cf700ca45ff2fbbcc6160a7d97ab6f06218
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2020-09-06 15:24:41 +0545

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	purge: [],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#130f40',
+				secondary: {
+					300: '#eb4d4b',
+					800: '#b71540'
+				}
+			},
+			fontFamily: {
+				body: ['Nunito']
+			}
+		}
+	},
+	variants: {},
+	plugins: []
+};
